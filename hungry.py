e1f87a0c78930d0fc61b44b30e446c873bd72ba7
aditya2kahol <aditya2kahol@gmail.com>
2023-08-27 15:16:58 +0530

    made changes to hungry.py

--- hungry.py
+++ hungry.py
@@ -1,9 +1,20 @@
+"""
+    ARE YOU HUNGRY MY CHILD ??
+"""
+
 hungry = input("Are you hungry?: ")
+
+food = []
+with open('food_items.txt', 'r') as f:
+    for food_item in f:
+        food.append(food_item.strip())
+
 if hungry == "yes":
-    food = input("ohh, okay, so would you like to have a samosa ?")
-    if food  == 'yes':
+    query = input("Okayy..so what would you like to have?")
+    if query in food:
         print("Great, please hang on!")
     else:
-        print("Okay, let me cook something else then.")
+        print("Okay, since you don't know, let me list the things for you.")
+        print(f"{(', ').join(food)}")
 else:
     print("do your homework")
