62a22aa4039e893354d9bf692144be85c365ef67
aditya2kahol <aditya2kahol@gmail.com>
2023-08-27 15:04:36 +0530

    added food_items.txt and made changes to hungry.py

--- hungry.py
+++ hungry.py
@@ -1,5 +1,9 @@
 hungry = input("Are you hungry?: ")
 if hungry == "yes":
-    print("eat samosa")
+    food = input("ohh, okay, so would you like to have a samosa ?")
+    if food  == 'yes':
+        print("Great, please hang on!")
+    else:
+        print("Okay, let me cook something else then.")
 else:
     print("do your homework")
